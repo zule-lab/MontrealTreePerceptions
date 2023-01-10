4dae226809c8abba287884ca835cc30f66487991
Isabella Richmond <icrichmond@mun.ca>
2023-01-10 13:19:09 -0500

    targets framework

--- _targets.R
+++ _targets.R
@@ -21,12 +21,42 @@ snapshot()
 restore()
 
 
-# Scripts -----------------------------------------------------------------
-# tar_source('scripts')
-tar_source('scripts')
+
+# Targets: input ----------------------------------------------------------
+
+targets_input <- c(
+  
+  tar_file_read(mtl_eng_raw,
+           "input/survey/UrbanForestCommunitySurvey_Montreal_ENG_July-11-2022.csv",
+           read.csv(file = !!.x, skip = 1)),
+  
+  tar_file_read(mtl_fre_raw,
+           "input/survey/UrbanForestCommunitySurvey_Montreal_FRE_July-11-2022.csv",
+           read.csv(file = !!.x)),
+  
+  tar_target(
+    mtl_eng_clean,
+    clean_mtl_eng(mtl_eng_raw)
+  ),
+  
+  tar_target(
+    mtl_fre_clean,
+    clean_mtl_fre(mtl_fre_raw)
+  ),
+  
+  tar_target(
+    mtl_clean,
+    rbind(mtl_eng_clean, mtl_fre_clean) # need to remove duplicates? matching IP addresses across FR/EN surveys?
+  )
+  
+)
+
+
+# Targets: models ---------------------------------------------------------
+
 
 
 # Targets: all ------------------------------------------------------------
-# Automatically grab all the "targets" lists above
-
+# Automatically grab all the "targets_*" lists above
 lapply(grep('targets', ls(), value = TRUE), get)
+
