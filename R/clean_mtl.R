a1bd98997ed043d4481ece755910cbebc043e792
icrichmond <icrichmond@mun.ca>
2023-01-24 11:08:23 -0500

    changing vars based on narrowed question

--- R/clean_mtl.R
+++ R/clean_mtl.R
@@ -4,41 +4,38 @@ clean_mtl <- function(mtl_raw){
   
   mtl_sel <- mtl_cols %>% 
     select(c(id, recorded_date, location_latitude, location_longitude, postcode, language, in_which_city_or_municipal_boundary_do_you_live_in,
-             city_type, starts_with("uf_vals"), starts_with("beliefs"), starts_with("wtp"), starts_with("knowledge"), tree_in_front_of_house_original,
-             planted_a_tree_original, contact_council_for_trees_original, provide_feedback_new, starts_with("hours"), 
-             starts_with("tree"), starts_with("mgmt"), starts_with("city"), starts_with("social"), years_neigh, years_urban,
-             years_rural, housing_code, housing_type_of_dwelling, canborn, can_born_year_in_canada, canborn_par, esl, language_1, 
-             language_french_spoken, decade, age40, edu, edu_uni, envorg, ethnicity, ethnicity_other, gender_f))
+             starts_with("uf_vals"), starts_with("beliefs"), city_type, housing_code, housing_type_of_dwelling, 
+             canborn, canborn_par, esl, french_yes_1_1, language_french_spoken, edu))
   
   mtl_code <- mtl_sel %>%
     mutate(
-      years_neigh = as.numeric(years_neigh),
       
-      years_urban = as.numeric(years_urban),
+      city_type = case_when(city_type %in% "inner" == T ~ 1,
+                            city_type %in% "middle" == T ~ 2, 
+                            city_type %in% "outer" == T ~ 3, 
+                            city_type %in% "regional" == T ~ 4),
       
-      housing_type_of_dwelling = case_when(housing_type_of_dwelling %in% "House / Maison" == T ~ 0,
-                                           housing_type_of_dwelling %in% "Apartment house / Appartement maison" == T ~ 1,
-                                           housing_type_of_dwelling %in% "Apartment building / Appartement édifice" == T ~ 2,
-                                           housing_type_of_dwelling %in% "Other / Autre" == T ~ 3,
-                                           housing_type_of_dwelling %in% "Don’t know or Prefer not to answer / Je ne sais pas ou Je préfère ne pas répondre" == T ~ 4),
+      # housing code: 1 = own, 2 = rent, 3 = other
       
-      decade = case_when(decade %in% "20s" == T ~ 0,
-                         decade %in% "30s" == T ~ 1, 
-                         decade %in% "40s" == T ~ 2,
-                         decade %in% "50s" == T ~ 3, 
-                         decade %in% "60s" == T ~ 4,
-                         decade %in% "70s" == T ~ 5,
-                         decade %in% "80s" == T ~ 6, 
-                         decade %in% "90s" == T ~ 7,
-                         decade %in% "00s" == T ~ 8, 
-                         decade %in% "Don’t know / Prefer not to answer" == T ~ 9,
-                         decade %in% "Je ne sais pas / Je préfère ne pas répondre" == T ~ 9),
+      housing_type_of_dwelling = case_when(housing_type_of_dwelling %in% "House / Maison" == T ~ 1,
+                                           housing_type_of_dwelling %in% "Apartment house / Appartement maison" == T ~ 2,
+                                           housing_type_of_dwelling %in% "Apartment building / Appartement édifice" == T ~ 3,
+                                           housing_type_of_dwelling %in% "Other / Autre" == T ~ 4,
+                                           housing_type_of_dwelling %in% "Don’t know or Prefer not to answer / Je ne sais pas ou Je préfère ne pas répondre" == T ~ 5),
       
-      city_type = case_when(city_type %in% "inner" == T ~ 0,
-                            city_type %in% "middle" == T ~ 1, 
-                            city_type %in% "outer" == T ~ 2, 
-                            city_type %in% "regional" == T ~ 3)
+      immigrant = case_when(canborn %in% 1 & canborn_par %in% 1 ~ 1, # 2nd gen Canadian
+                            canborn %in% 1 & canborn_par %in% 0 ~ 2, # 1st gen Canadian
+                            canborn %in% 0 & canborn_par %in% 0 ~ 3, # 1st gen immigrant
+                            canborn %in% 0 & canborn_par %in% 1 ~ 4 # foreign born Canadian
+                            ),
       
+      firstlanguage = case_when(esl %in% 1 & language_french_spoken %in% 0 ~ 1, # french first language
+                                esl %in% 0 ~ 2, # english first language
+                                esl %in% 1 & language_french_spoken %in% 0 & french_yes_1_1 %in% 1 ~ 3, # french second language
+                                esl %in% 1 & language_french_spoken %in% 0 & french_yes_1_1 %in% 0 ~ 4# english second language
+                                ) 
+      
+      # edu: 0 = prefer not to answer, 1 = did not complete high school, 2 = high school, 3 = trade school, 4 = bac, 5 = masters, 6 = doc
       
       )
   
