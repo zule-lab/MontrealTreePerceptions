430631027b7d4c3f9f87ddd1504f3020e4aa7d04
icrichmond <icrichmond@mun.ca>
2023-10-18 10:00:09 -0400

    add prior only and cmdstanr models

--- _targets.R
+++ _targets.R
@@ -51,6 +51,8 @@ c(
   ),
   
   # explore variance using random effects model 
+  # note: models are misspecified 
+  # TODO: add zarg
   tar_target(
     init_model,
     brm(bf(score ~ 1 + (1|id) + (1|question) + (1|question_category)),
@@ -63,8 +65,39 @@ c(
                   prior(exponential(4), class = "sd")),
         cores = getOption("mc.cores", 8),
         chains = 4,
+        backend = 'rstan')
+    ),
+  
+  tar_target(
+    init_model_prior,
+    brm(bf(score ~ 1 + (1|id) + (1|question) + (1|question_category)),
+        data = mtl_long,
+        family = cumulative(),
+        prior = c(prior(normal(-2.5, 0.4),  class = "Intercept", coef = "1"),
+                  prior(normal(-1.5, 0.4),  class = "Intercept", coef = "2"),
+                  prior(normal(-0.5, 0.2),   class = "Intercept", coef = "3"),
+                  prior(normal(0.5, 0.4),   class = "Intercept", coef = "4"),
+                  prior(exponential(4), class = "sd")),
+        sample_prior = "only",
+        cores = getOption("mc.cores", 8),
+        chains = 4,
         backend = 'cmdstanr')
-    )
+  ),
+  
+  tar_target(
+    init_model_cmdstanr,
+    brm(bf(score ~ 1 + (1|id) + (1|question) + (1|question_category)),
+        data = mtl_long,
+        family = cumulative(), 
+        prior = c(prior(normal(-2.5, 0.4),  class = "Intercept", coef = "1"),
+                  prior(normal(-1.5, 0.4),  class = "Intercept", coef = "2"),
+                  prior(normal(-0.5, 0.2),   class = "Intercept", coef = "3"),
+                  prior(normal(0.5, 0.4),   class = "Intercept", coef = "4"),
+                  prior(exponential(4), class = "sd")),
+        cores = getOption("mc.cores", 8),
+        chains = 4,
+        backend = 'cmdstanr')
+  )
   
 
   
