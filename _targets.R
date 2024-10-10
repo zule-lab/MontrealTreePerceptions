573384bda515b808c99f9e263cfeed1de74997e3
icrichmond <bellarichmond@proton.me>
2024-10-10 11:58:25 -0400

    add tree satisfaction, start workign through cumulative probit

--- _targets.R
+++ _targets.R
@@ -43,38 +43,39 @@ c(
   tar_target(
     mtl_long,
     mtl_clean %>%
-      pivot_longer(cols = starts_with(c('uf_vals', 'beliefs'))) %>%
+      pivot_longer(cols = starts_with(c('uf_vals', 'beliefs', 'tree_satisfaction'))) %>%
       mutate(question = name,
              score = value,
              question_category = case_when(startsWith(question, 'uf_vals') ~ 'values',
                                            startsWith(question, 'beliefs_negative') ~ 'negative_beliefs',
                                            startsWith(question, 'beliefs_extra_negative') ~ 'extra_negative_beliefs',
                                            startsWith(question, 'beliefs_positive') ~ 'positive_beliefs',
-                                           startsWith(question, 'beliefs_extra_positive') ~ 'extra_positive_beliefs'))
+                                           startsWith(question, 'beliefs_extra_positive') ~ 'extra_positive_beliefs',
+                                           startsWith(question, 'tree_satisfaction') ~ 'tree_satisfaction'))
     
   ),
   
   # explore variance using random effects model 
   # note: models are misspecified 
-  # change exponential prior to normal(0, 0,2)
-  # do predictions with prior only model - is the variation in question and individual representative of the dataset
-  # - we want a plausible range that is close to real data 
+  # change exponential prior to normal(0, 0.2)
   
   zar_brms(
     language_de,
-    bf(score ~ 1 + firstlanguage + city_type + ethncode + decade_num + (1|id) + (1|question)),
-    family = cumulative(),
-    prior = c(prior(normal(0, 0.5), class = "b"),
-              prior(normal(-2.5, 0.4), class = "Intercept", coef = "1"),
-              prior(normal(-1.5, 0.4),  class = "Intercept", coef = "2"),
-              prior(normal(-0.5, 0.2),   class = "Intercept", coef = "3"),
-              prior(normal(0.5, 0.4),   class = "Intercept", coef = "4"),
-              prior(exponential(1), class = "sd")),
+    #bf(score ~ 1 + firstlanguage + city_type + ethncode + decade_num + (1|id) + (1|question)),
+    bf(score ~ 1),
+    family = cumulative(probit),
+    prior = c(#prior(normal(0, 0.5), class = "b"),
+              prior(normal(-1, 0.5), class = "Intercept", coef = "1"),
+              prior(normal(-0.5, 0.5),  class = "Intercept", coef = "2"),
+              prior(normal(0, 0.5),   class = "Intercept", coef = "3"),
+              prior(normal(0.5, 0.5),   class = "Intercept", coef = "4")),
+              #prior(normal(1, 0.5), class = "sd")),
     backend = 'cmdstanr',
     data = mtl_long,
     chains = 4,
     iter = 1000,
-    cores = 4
+    cores = 4,
+    init = 0.2
     )
 
   
